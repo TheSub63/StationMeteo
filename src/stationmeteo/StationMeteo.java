1959b52e50f816285673a59452bcd345e75cc9d4
Clement <clement.GUILBERT@etu.uca.fr>
2017-11-17 12:05:43 +0100

    Dernier COmmit 17 nov

--- src/stationmeteo/StationMeteo.java
+++ src/stationmeteo/StationMeteo.java
@@ -5,6 +5,7 @@
  */
 package stationmeteo;
 
+import java.io.InputStream;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -12,40 +13,41 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 
 /**
  *
  * @author clguilbert
  */
 public class StationMeteo extends Application {
-    
+    private Stage primaryStage;
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        this.primaryStage=primaryStage;
+        Capteur cap1 = (Capteur) replaceSceneContent("fenetreCapteur.fxml");
+        cap1.setApp(this);
+        primaryStage.setTitle("Capteur 0");
         primaryStage.show();
     }
 
+    
+    private Initializable replaceSceneContent(String fxml){
+        FXMLLoader loader = new FXMLLoader();
+//        TabPane page=fenetreCapteur();
+//        Scene scene = new Scene(page, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        return (Initializable) loader.getController();
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(StationMeteo.class, (java.lang.String[])null);
     }
     
 }
