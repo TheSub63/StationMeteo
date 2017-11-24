ddc444fd5ffa0be7f53f0b0cf54c6c49984767bf
Clement <clement.GUILBERT@etu.uca.fr>
2017-11-24 12:04:57 +0100

    Commit  midi

--- src/stationmeteo/controller/MainController.java
+++ src/stationmeteo/controller/MainController.java
@@ -5,13 +5,20 @@
  */
 package stationmeteo.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.application.Preloader;
 import javafx.application.Preloader.ProgressNotification;
 import javafx.application.Preloader.StateChangeNotification;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import stationmeteo.java.StationMeteo;
 
 /**
  * Simple Preloader Using the ProgressBar Control
@@ -20,12 +27,24 @@ import javafx.stage.Stage;
  */
 public class MainController extends BorderPane implements Initializable {
     
-
-    Stage stage;
+    @FXML
+    Button addButton;
+    @FXML
+    Button uptButton;
+    @FXML
+    Button delButton;
+    @FXML
+    Label nameText;
     
-    private Scene createPreloaderScene() {
-        BorderPane p = new BorderPane();
-        return new Scene(p, 300, 150);        
+ private StationMeteo application;
+    
+    public void setApp(StationMeteo application){
+        this.application = application;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
         
